a021c3d0e4b361ccbd7d904bc35302ba98de45a6
MangoEric <fgh1937@yahoo.com>
2024-04-10 21:06:13 +0900

    [boards] Add create-board.dto

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,9 @@ export class BoardsService {
     return this.boards;
   }
 
-  createBoard(title: string, description: string) {
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+
     const board: Board = {
       id: uuid(), // uuid 패키지를 사용하여 고유한 id 생성
       title, // title: title과 같은 의미
