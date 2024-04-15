51e44c1d3115997a3e99ab77c1559b261a243bda
MangoEric <fgh1937@yahoo.com>
2024-04-16 00:11:01 +0900

    [auth] add Auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: UserRepository,
+  ) {
+  }
+}
