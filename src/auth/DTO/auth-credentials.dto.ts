e362584b7eb1868b26ee9395cf4fad35c63ff1b9
MangoEric <fgh1937@yahoo.com>
2024-04-16 20:11:26 +0900

    [auth] Add ValidationPipe

--- src/auth/DTO/auth-credentials.dto.ts
+++ src/auth/DTO/auth-credentials.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
-    username: string;
-    password: string;
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // 영어랑 숫자만 가능한 정규식 추가
+  @Matches(
+    /^[a-zA-Z0-9]*$/, 
+    { message: 'password only accepts english and number' }
+  )
+  password: string;
 }
