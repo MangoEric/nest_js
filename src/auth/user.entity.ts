049bf6525da3dfbaf3fa62d877c4e047ebf23077
MangoEric <fgh1937@yahoo.com>
2024-04-21 00:24:25 +0900

    [entity] Add OneToMany & ManyToOne

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from 'typeorm';
+import { Board } from '../boards/board.entity';
 
 @Entity()
 @Unique(['username'])
@@ -11,4 +12,7 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany(type => Board, board => board.user, { eager: true })
+  boards: Board[];
 }
