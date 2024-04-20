acb004b6c1bbcc0f67274ab3e6a82b981f09910a
MangoEric <fgh1937@yahoo.com>
2024-04-20 18:03:10 +0900

    [boards] add auth in boards

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,12 +4,14 @@ import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [BoardsController],
   providers: [BoardsService],
   imports: [
-    TypeOrmModule.forFeature([Board, BoardRepository])
+    TypeOrmModule.forFeature([Board, BoardRepository]),
+    AuthModule
   ]
 })
 export class BoardsModule {}
